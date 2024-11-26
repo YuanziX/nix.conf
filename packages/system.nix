7ef48052ea2daf60632d73cdb8d24e67be8f3944
YuanziX <achubadyal4@gmail.com>
2024-11-26 20:50:35 +0530

    feat: add wifi manager applet for waybar

--- packages/system.nix
+++ packages/system.nix
@@ -1,8 +1,10 @@
 { pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    
-    # bluetooth
+  environment.systemPackages = with pkgs; [  
+    # bluetooth manager
     blueman
+
+    # sys tray network applet
+    networkmanagerapplet
 
     # terminal
     kitty
