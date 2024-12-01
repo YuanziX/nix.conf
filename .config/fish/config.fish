1adb06ff35c5a0c11c6bcdf2448845ac7918d4c2
YuanziX <achubadyal4@gmail.com>
2024-12-01 14:43:58 +0530

    fish: add some abbrs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# cd help
+abbr .. cd ..
+abbr ... cd ../..
+abbr .... cd ../../..
+abbr ..... cd ../../../..
+abbr ..... cd ../../../../..
+
+# git abbrs
+abbr cp git cherry-pick
