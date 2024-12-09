e7267d6a8cdbc1aa44a408cbadaafa5b143a7861
YuanziX <achubadyal4@gmail.com>
2024-12-09 20:07:11 +0530

    nix: use cachix

---
+++ cachix.nix
@@ -0,0 +1,7 @@
+{
+  # walker
+  nix.settings = {
+    substituters = [ "https://walker.cachix.org" ];
+    trusted-public-keys = [ "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM=" ];
+  };
+}
--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,7 @@
 {
   imports = [
     # Include the results of the hardware scan.
+    ./cachix.nix
     ./core
     ./packages
     ./programs
